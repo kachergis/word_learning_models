5687e561c1d655db166752b8fd62e019d3582b3c
kachergis <george.kachergis@gmail.com>
2015-06-30 01:15:02 -0400

    fixed Fazly et al 2010 model

--- models/fazly.R
+++ models/fazly.R
@@ -1,79 +1,60 @@
-# reimplemented following Fazly et al.'s code 
-# George Kachergis March 17, 2015
+# reimplemented following Fazly et al.'s code + paper
+# George Kachergis June 11, 2015
 
-model <- function(params, ord=c(), reps=1) {
-	lambda <- params[1] # small smoothing factor (1e-5)
-	beta <- params[2] # upper bound on number of symbol types to expect? (8500)
-	theta <- params[3] # threshold for knowledge (.7)
-	
-	# for alignment prob calculation Fazly et al use two extra fixed parameters for smoothing:
+model <- function(params, ord=c(), ord_name="", reps=1, name="fazly") {
+  lambda <- params[1] # small smoothing factor (1e-5)
+  beta <- params[2] # upper bound on number of symbol types to expect? (8500)
+  #theta <- params[3] # threshold for knowledge (.7)
+  
+  # for alignment prob calculation Fazly et al use two extra fixed parameters for smoothing:
   # (lines 174-5 learn.py)
-	alpha = 10
-	epsilon = 0.001
+  alpha = 10
+  epsilon = 0.001
+  voc_sz = max(unlist(ord), na.rm=TRUE) # vocabulary size
+  ref_sz = max(unlist(ord), na.rm=TRUE) # number of objects
+  traj = matrix(0, length(ord$trials), voc_sz) # list()
+  align = matrix(0, voc_sz, voc_sz) # calc from probs of stim on trial
+  assoc = matrix(0, voc_sz, voc_sz) # sum of alignments over time
+  probs = matrix(0, voc_sz, voc_sz) # normalized assocs
   
-	traj = list()
-	voc_sz = max(unlist(ord$words), na.rm=TRUE) # vocabulary size
-	ref_sz = max(unlist(ord$objs), na.rm=TRUE) # number of objects
-	dummy = voc_sz + 1
-	m <- matrix(0, voc_sz+1, ref_sz) # association matrix
-  t_unseen = rep(1/beta,voc_sz+1) # in transp - used for updating probs of unseen objs
-	alignSum = matrix(0, voc_sz+1, ref_sz)
+  t_unseen = rep(1/beta, voc_sz)
   
-	#assm <- matrix(0, voc_sz+1, ref_sz) # track assoc scores SEPARATELY
-	# training
-	for(rep in 1:reps) { # for trajectory experiments, train multiple times
-	  for(t in 1:nrow(ord$words)) {
-	  	#print(format(m, digits=3))
-		
-		tr_w = as.integer(ord$words[t,])
-		tr_w = tr_w[!is.na(tr_w)]
-		tr_o = as.integer(ord$objs[t,])
-		tr_o = tr_o[!is.na(tr_o)]
-		
-		# calculate word-referent alignment probabilities
-		utt <- c(tr_w,dummy) # add dummy to utterance U(t)
-		# following Fazly's updateMappingTables code (learn.py line 168)
-		align = matrix(0, voc_sz+1, ref_sz) # align is their 'alignp' (they have alignp.sum and alignp.value)
-    for(f in tr_o) {
-      sumT = 0
-      for(w in utt) {
-        if(m[w,f]==0) {  # transp.getValue(w,f): unseen or m[w,f]
-          sumT = sumT + t_unseen[w]
-        } else {
-          sumT = sumT + m[w,f]
+  #assm <- matrix(0, voc_sz+1, ref_sz) # track assoc scores SEPARATELY
+  # training
+  for(rep in 1:reps) { # for trajectory experiments, train multiple times
+    for(t in 1:length(ord$trials)) {
+      
+      tr = as.integer(ord$trials[[t]]$words)
+      tr = tr[!is.na(tr)]
+      #tr_o = as.integer(ord$trials[[t]]$objs)
+      #tr_o = tr_o[!is.na(tr_o)]
+      
+      align = matrix(0, voc_sz, voc_sz) # calc from probs of stim on trial
+      tr_probs = probs
+        for(f in tr) {
+          for(w in tr) {
+            if(tr_probs[w,f]==0) {
+              tr_probs[w,f] = t_unseen[w]
+            } 
+          }
+          sumT = sum(tr_probs[tr,f]) + alpha*epsilon
+          for(w in tr) {
+            align[w,f] = (tr_probs[w,f]+epsilon) / sumT 
+          }
         }
-      }
-      for(w in utt) {
-        # m[w,f] = transp.getValue: unseen or m[m,f]
-        if(m[w,f]==0) {
-          tmp = t_unseen[w]
-        } else {
-          tmp = m[w,f]
+        assoc[tr,tr] = assoc[tr,tr] + align[tr,tr]
+        #probs[tr,] = assoc[tr,] / (rowSums(assoc[tr,])+beta*lambda) 
+        for(w in tr) {
+          sumA = sum(assoc[w,])
+          denom = sumA + beta*lambda
+          probs[w,] = (assoc[w,] + lambda) / denom
+          t_unseen[w] = lambda / denom
         }
-        align[w,f] = tmp + epsilon / (sumT + alpha*epsilon) # m is their 'transp'
-        alignSum[w,f] = alignSum[w,f] + align[w,f] # alignp.setValue and alignp.setSum
-      }
+      index = (rep-1)*length(ord$trials) + t # index for learning trajectory
+      traj[index,] = diag(probs) + 1e-9
     }
-		# from Fazly code (learn.py lines 193-204)
-    # "update translation probs for words in current sentence,
-		# taking into account all semantic primitives seen so far"
-    for(w in utt) {
-      sumA = sum(alignSum[w,]) # for all seen (allprimsD) -- tho alignp.prob was cleared
-      denom = sumA + beta*lambda
-      for(f in 1:ref_sz) { # again for all observed referents
-        m[w,f] = (alignSum[w,f] + lambda) / denom
-      }
-      t_unseen[w] = lambda / denom
-    }
-    
-		index = (rep-1)*length(ord$trials) + t # index for learning trajectory
-		traj[[index]] = m
-	  }
-	  comp = diag(m) / rowSums(m)[1:voc_sz]
-	}
-	minprob = 1/beta
-  #m[which(m<minprob)] = 0
-	perf = diag(m) / rowSums(m)[1:voc_sz] # not using theta threshold, but it only converts to binary know/not (can't help)
-	want = list(perf=perf, matrix=m[1:voc_sz,], traj=traj)
-	return(want)
+  }    
+  #perf = diag(m)[1:voc_sz] / rowSums(m)[1:voc_sz] # not using theta threshold, but it only converts to binary know/not (can't help)
+  want = list(perf=diag(probs), matrix=probs, traj=traj)
+  return(want)
 } 
